2520994bcb860661135f4d6a39f3341a321b0a98
Near32 <denamganai.kevin@gmail.com>
2017-01-13 03:09:20 +0900

    obstacle detection node : OPUSim_Obstacles : compilation-proof + test-proof

---
+++ build/CMakeFiles/OPUSim_Obstacles.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/OPUSim_Obstacles.dir/src/mainOPUSim_Obstacles.cpp.o"
+  "../bin/OPUSim_Obstacles.pdb"
+  "../bin/OPUSim_Obstacles"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/OPUSim_Obstacles.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ src/mainOPUSim_Obstacles.cpp
@@ -0,0 +1,37 @@
+#include "./OPUSim_Obstacles/OPUSim_Obstacles.h"
+
+int main(int argc, char* argv[])
+{
+	
+	
+	std::cout << " Usage : robot_number [int] // method [int=0] // verbose [int=0]." << std::endl;
+	
+	int robot_number = 0;
+	if(argc>1)
+	{
+		robot_number = atoi(argv[1]);
+	}
+	
+	int method = 0;	//ordered standard clipping
+	if( argc>2)
+	{
+		method = atoi(argv[2]);
+	}
+	
+	bool verbose = false;
+	if( argc>3)
+	{
+		verbose = (atoi(argv[3])==1?true:false);
+	}
+	
+	
+	ros::init(argc, argv,std::string("OPUSim_Obstacles_"+std::to_string(robot_number)).c_str() );
+
+	OPUSim_Obstacles obs(robot_number,method,verbose);
+	
+	ros::spin();
+	
+	return 0;
+	
+}
+
