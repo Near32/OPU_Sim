ce52215a14254dde850c377a927383830e3ecd34
Denamganai Kevin <denamganai.kevin@gmail.com>
2017-07-03 11:31:52 +0900

    creation of a bash launch file to launch the NLP experimentations.

--- launch/robot2swarm.launch.sh
+++ launch/robot2swarm.launch.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 source /home/kevin/rosbuild_ws/setup.bash
-roslaunch OPUSim robot.launch robot_number:=0 R:=1.0 emergencyBreak:=0 Omega:=2.0 tresholdDistAccount:=0.6 a:=1.0 kv:=0.1 kw:=0.2 epsilon:=1.0 debugRSO:=0 &
-roslaunch OPUSim robot.launch robot_number:=1 R:=1.0 emergencyBreak:=0 Omega:=2.0 tresholdDistAccount:=0.6 a:=1.0 kv:=0.1 kw:=0.2 epsilon:=1.0 debugRSO:=0 &
+roslaunch OPUSim world.spawn.launch &
+roslaunch OPUSim robot.spawn.launch robot_number:=0 X:=0.0 Y:=2.0 &
+roslaunch OPUSim robot.spawn.launch robot_number:=1	X:=0.5 Y:=-1.5 &
+roslaunch OPUSim obstacle.spawn.launch obstacle_number:=0	X:=2.2 Y:=0.0 &
+roslaunch OPUSim obstacle.spawn.launch obstacle_number:=1	X:=-2.8 Y:=0.0 &
+roslaunch OPUSim robot.launch robot_number:=0 R:=2.0 emergencyBreak:=0 Omega:=2.0 tresholdDistAccount:=0.6 a:=1.5 kv:=0.1 kw:=0.2 kR:=10.0 epsilon:=1.0 debugRSO:=0 Iang:=0.4 &
+roslaunch OPUSim robot.launch robot_number:=1 R:=2.0 emergencyBreak:=0 Omega:=2.0 tresholdDistAccount:=0.6 a:=1.5 kv:=0.1 kw:=0.2 kR:=10.0 epsilon:=1.0 debugRSO:=0 Iang:=0.4 &
+rosservice call /gazebo/unpause_physics &
